892cbce69e2b6bd4f64c9408cde09b52b70954aa
action <action@github.com>
2021-06-08 18:14:27 +0000

    Github Actions Automatically Publish gaia-starter in 2021-06-08 18:14

---
+++ gaia-starter/start-pi.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo '######################################'
+echo '     _____          _____          	  \n    / ____|   /\   |_   _|   /\    	  \n   | |  __   /  \    | |    /  \   	  \n   | | |_ | / /\ \   | |   / /\ \  	  \n   | |__| |/ ____ \ _| |_ / ____ \    \n    \_____/_/    \_\_____/_/    \_\ \n'
+echo '######################################\n'
+echo 'Start...\n'
+
+# Get Docker Images
+echo 'Pull docker images\n'
+docker pull pigtastic/gaia-ui
+docker pull pigtastic/gaia-server
+docker pull andresvidal/rpi3-mongodb3
+
+echor "Create mongodb directory"
+ mkdir -p ~/mongo/data
+
+# Start Containers
+echo '\nStart-up\n'
+docker-compose -p gaia-home --profile pi up --detach
+
+echo '\n FINISH'
